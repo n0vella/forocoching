7f27f89fe12157f5cb7ed46f23d8ecc3c9d10a3a
n0vella <n0vella@outlook.com>
2025-08-03 11:39:53 +0200

    formatted log function

--- src/content-script/index.ts
+++ src/content-script/index.ts
@@ -1,8 +1,14 @@
+import manifest from "../../manifest.json"
+
 const myConsole = window.console
+
+function log(...data: any[]) {
+  console.log(`%c${manifest.name}:`, "color: green; font-weight: bold", ...data)
+}
 
 function main() {
   window.console = myConsole // return hijacked console
-  window.log = console.log // convenience
+  window.log = log // convenience
 
   log("Hello world!")
 }
